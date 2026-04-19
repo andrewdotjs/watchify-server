6098cc365e8537b6b6df06677992e4e2fee223d3
Andrew Williams <Andrew Williams>
2026-04-19 13:32:00 -0700

    update

--- internal/handlers/covers/read.go
+++ internal/handlers/covers/read.go
@@ -2,6 +2,7 @@ package covers
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -10,19 +11,20 @@ import (
 	"github.com/andrewdotjs/watchify-server/internal/logger"
 	"github.com/andrewdotjs/watchify-server/internal/placeholders"
 	"github.com/andrewdotjs/watchify-server/internal/responses"
+	"github.com/andrewdotjs/watchify-server/internal/types"
 	"github.com/google/uuid"
 )
 
-// Returns the covers stored in the database and file-system. If none are present,
+// Returns the covers stored in the database and filesystem. If none are present,
 // return a placeholder cover.
 //
 // # Specifications:
 //   - Method   : GET
-//   - Endpoint : /series/{id}/cover
+//   - Endpoint : /shows/{id}/cover
 //   - Auth?    : False
 //
 // # HTTP request path parameters:
-//   - id       : REQUIRED. UUID of the series.
+//   - id       : REQUIRED. UUID of the show.
 func Read(
     w http.ResponseWriter,
     r *http.Request,
@@ -32,8 +34,8 @@ func Read(
 ) {
 	var id string = r.PathValue("id")
 	var uploadDirectory string = path.Join(*appDirectory, "storage", "covers")
-	var coverFileName string = ""
 	var functionId string = uuid.NewString()
+	var cover types.Cover = types.Cover{}
 
 	if id == "" {
 	  log.Info(functionId, "Requested cover did not exist, sending placeholder.")
@@ -47,27 +49,45 @@ func Read(
 
 	if err := database.QueryRow(
 		`
-		  SELECT
-				file_name
-			FROM
-			  covers
-			WHERE
-			  parent_id=?
+		SELECT
+			id, file_extension, file_name, upload_date
+		FROM
+			movies
+		WHERE
+			id = ?
 		`,
 		id,
 	).Scan(
-		&coverFileName,
+		&cover.Id,
+		&cover.FileExtension,
+		&cover.FileName,
+		&cover.UploadDate,
 	); err != nil {
-	  log.Error(functionId, fmt.Sprintf("Database error, sending placeholder. %v", err))
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+		  log.Info(functionId, "No movie found with provided ID")
+			responses.Error{
+				Type:     "null",
+				Title:    "Data not found",
+				Status:   400,
+				Detail:   "No movie could be found with the given id.",
+				Instance: r.URL.Path,
+			}.ToClient(w)
+		default:
+		  log.Error(functionId, fmt.Sprintf("An unknown error occurred. %v", err))
+			responses.Error{
+				Type:     "null",
+				Title:    "Unknown Error",
+				Status:   500,
+				Detail:   fmt.Sprintf("%v", err),
+				Instance: r.URL.Path,
+			}.ToClient(w)
+		}
 
-		responses.File{
-			StatusCode: 200,
-			FileBuffer: placeholders.Cover(),
-		}.ToClient(w)
 		return
 	}
 
-	if buffer, err := os.ReadFile(path.Join(uploadDirectory, coverFileName)); err != nil {
+	if buffer, err := os.ReadFile(path.Join(uploadDirectory, cover.FileName)); err != nil {
 		log.Error(functionId, fmt.Sprintf("File system error, sending placeholder. %v", err))
 
 		responses.File{
